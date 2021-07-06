2c789190c8fd6f82f0d0f90317e17e9f041177fa
whwjdan <whwjdan@naver.com>
2021-07-07 00:00:45 +0900

    프로젝트 생성

---
+++ src/test/java/com/jjm_delivery/jjmdelivery/JjmDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjm_delivery.jjmdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjmDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
